d032a90d37b96f184766fe4b316471b2991df308
Ibr4him Abdiqadir <ibr4himabdiqadirali@gmail.com>
2026-09-12 16:00:57 +0300

    Fix syntax error in bot.py keyboard requests

--- bot.py
+++ bot.py
@@ -34,13 +34,13 @@ def km(i):
         {"text": "👥 Group", "request_chat": {"request_id": 4, "chat_is_channel": False, "request_title": True, "request_username": True, "request_photo": True}},
         {"text": "📢 Channel", "request_chat": {"request_id": 5, "chat_is_channel": True, "request_title": True, "request_username": True, "request_photo": True}},
         {"text": "💬 Forum", "request_chat": {"request_id": 6, "chat_is_channel": False, "chat_is_forum": True, "request_title": True, "request_username": True, "request_photo": True}}
-        {"text": "🤖 My bot", "request_users": {"request_id": 7, "user_is_bot": True, "request_name": True, "request_username": True, "request_photo": True}}
+        {"text": "🤖 My bot", "request_users": {"request_id": 7, "user_is_bot": True, "request_name": True, "request_username": True "request_photo": True}}
     ])
     
     kbd.append([
     {"text": "👥 My Group", "request_chat": {"request_id": 7, "chat_is_channel": False, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}},
     {"text": "📢 My Channel", "request_chat": {"request_id": 8, "chat_is_channel": True, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}},
-    {"text": "💬 My Forum", "request_chat": {"request_id": 9, "chat_is_channel": False, "chat_is_forum": True, "chat_is_created": True, "request_title": True, "request_username": True "request_photo": True}},
+    {"text": "💬 My Forum", "request_chat": {"request_id": 9, "chat_is_channel": False, "chat_is_forum": True, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}},
 
 ])
 
