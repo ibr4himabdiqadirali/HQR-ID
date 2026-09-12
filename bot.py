d7244f5ef2b57464360d0add82962424744a3e67
Ibr4him Abdiqadir <ibr4himabdiqadirali@gmail.com>
2026-09-12 16:00:17 +0300

    Fix syntax error in request_chat for My Forum

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ def km(i):
     kbd.append([
     {"text": "👥 My Group", "request_chat": {"request_id": 7, "chat_is_channel": False, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}},
     {"text": "📢 My Channel", "request_chat": {"request_id": 8, "chat_is_channel": True, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}},
-    {"text": "💬 My Forum", "request_chat": {"request_id": 9, "chat_is_channel": False, "chat_is_forum": True, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}},
+    {"text": "💬 My Forum", "request_chat": {"request_id": 9, "chat_is_channel": False, "chat_is_forum": True, "chat_is_created": True, "request_title": True, "request_username": True "request_photo": True}},
 
 ])
 
