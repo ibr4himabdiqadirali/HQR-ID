2bef4bdd23adbe22f683600342cc37e0d9572428
Ibr4him Abdiqadir <ibr4himabdiqadirali@gmail.com>
2026-09-12 15:16:01 +0300

    Update bot keyboard to include bot chat request

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ def km(i):
         {"text": "👥 My Group", "request_chat": {"request_id": 7, "chat_is_channel": False, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}},
         {"text": "📢 My Channel", "request_chat": {"request_id": 8, "chat_is_channel": True, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}},
         {"text": "💬 My Forum", "request_chat": {"request_id": 9, "chat_is_channel": False, "chat_is_forum": True, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}}
-        {"text": "🤖 My bot", "request_chat": {"request_id": 3, "chat_is_bot": False, "chat_is_created": True, "request_title": True, "request_username": True, "request_photo": True}},
+        {"text": "🤖 My bot","request_chat": {"request_id": 3,"chat_is_bot": True}}
 
     ])
     
